dbecda547c4073eaa674feb6d234abd04e779e94
tbz <401219981@qq.com>
2023-10-21 02:46:22 +0000

    Changes to be committed: 	modified:   nemu/src/monitor/debug/expr.c

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -48,10 +48,10 @@ void init_regex() {
    int reti;
 
    for (i = 0; i < NR_REGEX; i++) {
-      reti = regcomp( &re[i], rules[i].regex, REG_EXTENDED );
+      reti = regcomp(&re[i], rules[i].regex, REG_EXTENDED);
       if (reti != 0) {
-         regerror( reti, &re[i], error_msg, 128 );
-         panic( "regex compilation failed: %s\n%s", error_msg, rules[i].regex );
+         regerror(reti, &re[i], error_msg, 128);
+         panic("regex compilation failed: %s\n%s", error_msg, rules[i].regex);
       }
    }
 }
@@ -62,10 +62,10 @@ typedef struct token
    char str[32];
 } Token;
 
-static Token tokens[32] __attribute__( (used) ) = {}; // 用于存放识别过了的字符串
-static int nr_token __attribute__( (used) ) = 0;      // 识别过了的字符串的数量
+static Token tokens[32] __attribute__((used)) = {}; // 用于存放识别过了的字符串
+static int nr_token __attribute__((used)) = 0;      // 识别过了的字符串的数量
 
-static bool make_token( char* e ) {
+static bool make_token(char* e) {
    int position = 0;
    int i;
    regmatch_t pmatch;
@@ -75,18 +75,18 @@ static bool make_token( char* e ) {
    while (e[position] != '\0') {
       /* Try all rules one by one. */
       for (i = 0; i < NR_REGEX; i++) {
-         if (regexec( &re[i], e + position, 1, &pmatch, 0 ) == 0 && pmatch.rm_so == 0) {
+         if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
             char* substr_start = e + position; // 当前循环中被判定字符的指针
             int substr_len = pmatch.rm_eo;     // 当前循环中被判定字符的长度
 
-            Log( "match rules[%d] = \"%s\" at position %d with len %d: %.*s",
-               i, rules[i].regex, position, substr_len, substr_len, substr_start );
+            Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+               i, rules[i].regex, position, substr_len, substr_len, substr_start);
 
             position += substr_len; // 挪动e中指针，针对最外层while循环做改变
 
             // 检查数组tokens是否已满
-            if (nr_token >= sizeof( tokens ) / sizeof( Token )) {
-               printf( "token array is full, cannot insert more\n" );
+            if (nr_token >= sizeof(tokens) / sizeof(Token)) {
+               printf("token array is full, cannot insert more\n");
                return false;
             }
 
@@ -94,8 +94,8 @@ static bool make_token( char* e ) {
 
                tokens[nr_token].type = rules[i].token_type; // 设置token类型
                // 将匹配的子字符串复制到token的str字段中
-               int length_to_copy = substr_len < sizeof( tokens[nr_token].str ) ? substr_len : sizeof( tokens[nr_token].str ) - 1;
-               strncpy( tokens[nr_token].str, substr_start, length_to_copy );
+               int length_to_copy = substr_len < sizeof(tokens[nr_token].str) ? substr_len : sizeof(tokens[nr_token].str) - 1;
+               strncpy(tokens[nr_token].str, substr_start, length_to_copy);
                tokens[nr_token].str[length_to_copy] = '\0';
             }
 
@@ -105,7 +105,7 @@ static bool make_token( char* e ) {
       }
 
       if (i == NR_REGEX) {
-         printf( "no match at position %d\n%s\n%*.s^\n", position, e, position, "" );
+         printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
          return false;
       }
    }
@@ -113,7 +113,7 @@ static bool make_token( char* e ) {
    return true;
 }
 
-int check_parentheses( int p, int q ) {
+int check_parentheses(int p, int q) {
    if (!(tokens[p].type == TK_LPAR && tokens[q - 1].type == TK_RPAR)) {
       return 0;
    }
@@ -137,10 +137,10 @@ int check_parentheses( int p, int q ) {
    return (count == 0);
 }
 
-eval( p, q ) {
+eval(p, q) {
    if (p > q) {
       /* Bad expression */
-      printf( "Bad expression from %d to %d.\n", p, q );
+      printf("Bad expression from %d to %d.\n", p, q);
       return -1;
    }
    else if (p == q) {
@@ -149,16 +149,16 @@ eval( p, q ) {
        * Return the value of the number.
        */
       if (tokens[p].type == TK_NUM) {
-         return (atoi( tokens[p].str ));
+         return (atoi(tokens[p].str));
       }
-      printf( "Unkown type %s.\n", tokens[p].type );
+      printf("Unkown type %s.\n", tokens[p].type);
       return -1;
    }
-   else if (check_parentheses( p, q ) == true) {
+   else if (check_parentheses(p, q) == true) {
       /* The expression is surrounded by a matched pair of parentheses.
        * If that is the case, just throw away the parentheses.
        */
-      return eval( p + 1, q - 1 );
+      return eval(p + 1, q - 1);
    }
    else {
       int op = -1;//principal operator
@@ -171,8 +171,8 @@ eval( p, q ) {
             break;
          }
       }
-      val1 = eval( p, op - 1 );
-      val2 = eval( op + 1, q );
+      val1 = eval(p, op - 1);
+      val2 = eval(op + 1, q);
 
       switch (op_type) {
       case '+': return val1 + val2;
@@ -180,20 +180,20 @@ eval( p, q ) {
       case '*': return val1 * val2;
       case '/': if (val2 != 0) { return val1 / val2; }
               else {
-         printf( "Division by zero.\n" );
+         printf("Division by zero.\n");
          return -1;
       }
-      default: assert( 0 );
+      default: assert(0);
       }
    }
 }
 
-word_t expr( char* e, bool* success ) {
-   if (!make_token( e )) {
+word_t expr(char* e, bool* success) {
+   if (!make_token(e)) {
       *success = false;
       return 0;
    }
-   return eval( 0, nr_token - 1 );
+   return eval(0, nr_token - 1);
 
    return 0;
 }
