1e5fbb765460ea6e5bd39fc005a7410a35923d58
tbz <401219981@qq.com>
2023-10-30 15:21:36 +0000

    Changes to be committed: 	modified:   nemu/tools/gen-expr/gen-expr.c

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -10,10 +10,10 @@ static char buf[65536] = {};
 static char code_buf[65536 + 128] = {}; // a little larger than `buf`
 static char* code_format =
 "#include <stdio.h>\n"
-"int main() { "
-"  unsigned result = %s; "
-"  printf(\"%%u\", result); "
-"  return 0; "
+"int main() {\n "
+"  unsigned result = %s;\n "
+"  printf(\"%%u\", result);\n "
+"  return 0;\n "
 "}";
 
 //generate a number less than n
@@ -21,8 +21,14 @@ int choose(int n) {
    return rand() % n;
 }
 
+void insert_rand_space() {
+   if (choose(2) && strlen(buf) + 2 < sizeof(buf)) {
+      strcat(buf, " ");
+   }
+}
+
 void gen_num() {
-   if (strlen(buf) + 1 < sizeof(buf)) {
+   if (strlen(buf) + 100 < sizeof(buf)) {
       //buf[strlen(buf)] = '0' + choose(10);//这个也对，但感觉最好不要这么写，容易错
       sprintf(buf + strlen(buf), "%d", choose(10));//这里buf + strlen(buf)指针被改变前存放\0
    }
@@ -30,7 +36,7 @@ void gen_num() {
 
 void gen_rand_op() {
    char ops[] = "+-*/";
-   if (strlen(buf) + 1 < sizeof(buf)) {
+   if (strlen(buf) + 100 < sizeof(buf)) {
       buf[strlen(buf)] = ops[choose(4)];
    }
 }
@@ -38,16 +44,29 @@ void gen_rand_op() {
 void gen_rand_exp() {
    switch (choose(3)) {
    case 0:
-      gen_num(); break;
+      gen_num();
+      break;
    case 1:
-      strcat("(");
+      strcat(buf, "(");
       gen_rand_exp();
-      strcat(")");
+      //gen_rand_op();
+      //gen_rand_exp();
+      strcat(buf, ")");
       break;
    default:
       gen_rand_exp();
       gen_rand_op();
-      gen_rand_exp();
+      // if we are generating a division operation, make sure the divisor is not zero.
+      if (buf[strlen(buf) - 1] == '/') {
+         int divisor;
+         do {
+            divisor = choose(10);
+         } while (divisor == 0);
+         sprintf(buf + strlen(buf), "%d", divisor);
+      }
+      else {
+         gen_rand_exp();
+      }
       break;
    }
 }
@@ -55,7 +74,7 @@ void gen_rand_exp() {
 int main(int argc, char* argv[]) {
    int seed = time(0);
    srand(seed);
-   int loop = 1;
+   int loop = 2;
    if (argc > 1) {
       sscanf(argv[1], "%d", &loop);
    }
