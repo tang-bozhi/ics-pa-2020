06ec9452884244770e2e0fac39ef0b46990457a9
tbz <401219981@qq.com>
2023-11-17 08:49:59 +0000

    Changes to be committed: 	modified:   nemu/tools/gen-expr/gen-expr.c

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -1,9 +1,11 @@
+//pa1-5表达式求值-如何测试你的代码-暂时认为完成
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <assert.h>
 #include <string.h>
+#include <regex.h>
 
 // this should be enough
 static char buf[65536] = {};
@@ -28,32 +30,68 @@ void insert_rand_space() {
 }
 
 void gen_num() {
+   insert_rand_space();
    if (strlen(buf) + 100 < sizeof(buf)) {
       //buf[strlen(buf)] = '0' + choose(10);//这个也对，但感觉最好不要这么写，容易错
       sprintf(buf + strlen(buf), "%d", choose(10));//这里buf + strlen(buf)指针被改变前存放\0
    }
+   insert_rand_space();
 }
 
 void gen_rand_op() {
+   insert_rand_space();
    char ops[] = "+-*/";
    if (strlen(buf) + 100 < sizeof(buf)) {
       buf[strlen(buf)] = ops[choose(4)];
    }
+   insert_rand_space();
+}
+
+//下方使用正则表达式过滤"[0-9]+\\("情况
+static regex_t regex;
+static int ret;
+static regmatch_t matchs[2];//保存匹配结果 
+
+void init_regex() {
+   ret = regcomp(&regex, "\\d+\\(|\\)\\d+", REG_EXTENDED);
+   assert(ret == 0);  // 确保正则表达式编译成功
+}
+
+void adding_rand_op() {
+   int offset = 0;
+   char ops[] = "+-*/";
+   init_regex();
+   while (!regexec(&regex, buf + offset, 1, matchs, 0)) {
+      memmove(buf + offset + matchs[0].rm_eo, buf + offset + matchs[0].rm_eo, sizeof(buf) - offset - matchs[0].rm_eo);
+      buf[offset + matchs[0].rm_eo] = ops[choose(4)];
+      offset += matchs[0].rm_eo + 1;
+   }
+   insert_rand_space();
 }
 
 void gen_rand_exp() {
+   insert_rand_space();
+   static int deepth = 0;
+   static int max_deep = 100;
+   if (deepth >= max_deep) {
+      gen_num();
+      return;
+   }
    switch (choose(3)) {
    case 0:
+      deepth++;
       gen_num();
       break;
    case 1:
+      deepth++;
       strcat(buf, "(");
       gen_rand_exp();
-      //gen_rand_op();
-      //gen_rand_exp();
+      gen_rand_op();
+      gen_rand_exp();
       strcat(buf, ")");
       break;
    default:
+      deepth++;
       gen_rand_exp();
       gen_rand_op();
       // if we are generating a division operation, make sure the divisor is not zero.
@@ -74,13 +112,16 @@ void gen_rand_exp() {
 int main(int argc, char* argv[]) {
    int seed = time(0);
    srand(seed);
-   int loop = 2;
+   int loop = 1;
    if (argc > 1) {
       sscanf(argv[1], "%d", &loop);
    }
    int i;
    for (i = 0; i < loop; i++) {
+      memset(buf, 0, sizeof(buf));
       gen_rand_exp();
+
+      adding_rand_op();
 
       sprintf(code_buf, code_format, buf);
 
@@ -101,5 +142,6 @@ int main(int argc, char* argv[]) {
 
       printf("%u %s\n", result, buf);
    }
+   regfree(&regex);
    return 0;
 }
