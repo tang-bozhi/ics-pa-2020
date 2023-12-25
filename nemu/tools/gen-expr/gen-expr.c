ab868a1ca304aefa55943a56d7ea10428c71667e
tbz <401219981@qq.com>
2023-12-25 15:26:05 +0000

    Changes to be committed: 	modified:   nemu/tools/gen-expr/gen-expr.c

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -159,7 +159,7 @@ void gen_rand_expr() {
 int main(int argc, char* argv[]) {
    int seed = time(0);
    srand(seed);
-   int loop = 10000;
+   int loop = 100;
    if (argc > 1) {
       sscanf(argv[1], "%d", &loop);
    }
