7726974375ae70700b01ba13093fb47aa5097ad7
tbz <401219981@qq.com>
2024-01-19 03:59:16 +0000

    Changes to be committed: 	modified:   nemu/src/monitor/debug/watchpoint.c 	modified:   nemu/src/monitor/debug/watchpoint.h

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -4,19 +4,61 @@
 #define NR_WP 32
 
 static WP wp_pool[NR_WP] = {};
-static WP *head = NULL, *free_ = NULL;
+static WP* head = NULL, * free_ = NULL;
 
+static int next_wp_NO = 0; // 全局变量，用于追踪下一个监视点的序号
+//链表初始化
 void init_wp_pool() {
-  int i;
-  for (i = 0; i < NR_WP; i ++) {
-    wp_pool[i].NO = i;
-    wp_pool[i].next = &wp_pool[i + 1];
-  }
-  wp_pool[NR_WP - 1].next = NULL;
+   int i;
+   for (i = 0; i < NR_WP; i++) {
+      wp_pool[i].NO = i;
+      wp_pool[i].next = &wp_pool[i + 1];
+   }//链接链表
+   wp_pool[NR_WP - 1].next = NULL;//wp_pool[31] 第32个wp 置NULL
 
-  head = NULL;
-  free_ = wp_pool;
+   head = NULL;
+   free_ = wp_pool;
 }
 
 /* TODO: Implement the functionality of watchpoint */
+
+static int next_wp_NO = 0; // 全局变量，用于追踪下一个监视点的序号
+// 返回新的监视点: wp->expr清空 wp->next清除至NULL old_value=0 new_value = 0
+WP* new_wp() {
+   if (free_ == NULL) {
+      assert(0);  // 没有可用的空闲监视点
+   }
+
+   WP* wp = free_;  // 从空闲链表中获取一个监视点
+   free_ = free_->next;  // 更新空闲链表的头指针
+   wp->next = NULL;      // 清除新监视点的next指针
+
+   // 初始化新监视点的其他字段...
+   wp->NO = next_wp_NO; // 分配一个序号给新监视点
+   next_wp_NO = (next_wp_NO + 1) % NR_WP; // 循环序号
+
+   wp->enabled = true;    // 默认启用新监视点
+   memset(wp->expr, 0, sizeof(wp->expr));  // 清空表达式字符串
+   wp->old_value = 0;     // 初始化旧值
+   wp->new_value = 0;     // 初始化新值
+
+   return wp;  // 返回新的监视点
+}
+
+
+void free_wp(WP* wp) {
+   if (wp == NULL) {
+      return;  // 如果传入的监视点为空，则直接返回
+   }
+
+   // 重置监视点的部分字段，以便于重用
+   wp->enabled = false;    // 禁用监视点
+   memset(wp->expr, 0, sizeof(wp->expr)); // 清空表达式字符串
+   wp->old_value = 0;      // 重置旧值
+   wp->new_value = 0;      // 重置新值
+
+   // 将监视点插入到空闲链表的头部
+   wp->next = free_;       // 设置当前监视点的next指向当前的空闲链表头
+   free_ = wp;             // 更新空闲链表的头指针
+}
 
--- nemu/src/monitor/debug/watchpoint.h
+++ nemu/src/monitor/debug/watchpoint.h
@@ -3,12 +3,14 @@
 
 #include <common.h>
 
-typedef struct watchpoint {
-  int NO;
-  struct watchpoint *next;
-
-  /* TODO: Add more members if necessary */
-
+typedef struct watchpoint {//watchpoint
+   int NO;                          // 监视点的序号
+   struct watchpoint* next;         // 下一个监视点的指针
+   /* TODO: Add more members if necessary */
+   char expr[256];                  // 监视的表达式
+   uint32_t old_value;              // 表达式的旧值
+   uint32_t new_value;              // 表达式的新值
+   bool enabled;                    // 监视点是否启用
 } WP;
 
 
