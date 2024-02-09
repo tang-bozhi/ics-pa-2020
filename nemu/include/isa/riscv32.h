30318a4bef2e6155978196ce2f085741d98dcd5e
TBZ <401219981@qq.com>
2024-02-09 07:29:22 +0000

    modified:   nemu/include/isa/riscv32.h

--- nemu/include/isa/riscv32.h
+++ nemu/include/isa/riscv32.h
@@ -23,19 +23,19 @@ typedef struct {
       struct {
          uint32_t opcode1_0 : 2;//opcode1_0 和 opcode6_2：指令操作码的一部分，用于指定指令的类型。
          uint32_t opcode6_2 : 5;
-         uint32_t rd : 5;
-         uint32_t funct3 : 3;
-         uint32_t rs1 : 5;
-         int32_t  simm11_0 : 12;
+         uint32_t rd : 5;//rd：目标寄存器，用于存储指令执行的结果。
+         uint32_t funct3 : 3;//funct3：功能码，与操作码一起确定指令的确切操作。
+         uint32_t rs1 : 5;//rs1：第一个源寄存器。
+         int32_t  simm11_0 : 12;//simm11_0：12位有符号立即数，用于算术和逻辑操作。
       } i;//I-type（立即数类型）
       struct {
          uint32_t opcode1_0 : 2;
          uint32_t opcode6_2 : 5;
-         uint32_t imm4_0 : 5;
+         uint32_t imm4_0 : 5;//imm4_0 和 simm11_5：存储指令中的立即数，用于确定存储位置的偏移量。
          uint32_t funct3 : 3;
          uint32_t rs1 : 5;
-         uint32_t rs2 : 5;
-         int32_t  simm11_5 : 7;
+         uint32_t rs2 : 5;//其他字段与I-type相似，但rs2代表第二个源寄存器，用于存储到内存的值。
+         int32_t  simm11_5 : 7;//imm4_0 和 simm11_5：存储指令中的立即数，用于确定存储位置的偏移量。
       } s;//S-type（存储类型）
       struct {
          uint32_t opcode1_0 : 2;
