4df578e97baa10aa409e496d0e942efa410b7683
TBZ <401219981@qq.com>
2024-02-16 05:51:44 +0000

    modified:   nemu/include/rtl/rtl.h 	modified:   nemu/include/common.h

--- nemu/include/rtl/rtl.h
+++ nemu/include/rtl/rtl.h
@@ -1,6 +1,6 @@
 #ifndef __RTL_RTL_H__
 #define __RTL_RTL_H__
-
+//RTL:Restor transmission language
 #include <cpu/decode.h>
 
 #define id_src1 (&s->src1)
@@ -24,25 +24,25 @@ void rtl_exit(int state, vaddr_t halt_pc, uint32_t halt_ret);
 
 // relation operation
 enum {
-  //            +-- unsign
-  //            |   +-- sign
-  //            |   |   +-- equal
-  //            |   |   |   +-- invert
-  //            |   |   |   |
-  RELOP_FALSE = 0 | 0 | 0 | 0,
-  RELOP_TRUE  = 0 | 0 | 0 | 1,
-  RELOP_EQ    = 0 | 0 | 2 | 0,
-  RELOP_NE    = 0 | 0 | 2 | 1,
+   //            +-- unsign
+   //            |   +-- sign
+   //            |   |   +-- equal
+   //            |   |   |   +-- invert
+   //            |   |   |   |
+   RELOP_FALSE = 0 | 0 | 0 | 0,
+   RELOP_TRUE = 0 | 0 | 0 | 1,
+   RELOP_EQ = 0 | 0 | 2 | 0,
+   RELOP_NE = 0 | 0 | 2 | 1,
 
-  RELOP_LT    = 0 | 4 | 0 | 0,
-  RELOP_LE    = 0 | 4 | 2 | 0,
-  RELOP_GT    = 0 | 4 | 2 | 1,
-  RELOP_GE    = 0 | 4 | 0 | 1,
+   RELOP_LT = 0 | 4 | 0 | 0,
+   RELOP_LE = 0 | 4 | 2 | 0,
+   RELOP_GT = 0 | 4 | 2 | 1,
+   RELOP_GE = 0 | 4 | 0 | 1,
 
-  RELOP_LTU   = 8 | 0 | 0 | 0,
-  RELOP_LEU   = 8 | 0 | 2 | 0,
-  RELOP_GTU   = 8 | 0 | 2 | 1,
-  RELOP_GEU   = 8 | 0 | 0 | 1,
+   RELOP_LTU = 8 | 0 | 0 | 0,
+   RELOP_LEU = 8 | 0 | 2 | 0,
+   RELOP_GTU = 8 | 0 | 2 | 1,
+   RELOP_GEU = 8 | 0 | 0 | 1,
 };
 
 #include <rtl-basic.h>
