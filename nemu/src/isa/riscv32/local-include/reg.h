f177778486f4382f8e14f93786e9e0e8d50ea0e8
TBZ <401219981@qq.com>
2024-03-03 06:05:48 +0000

    modified:   nemu/src/isa/riscv32/local-include/reg.h

--- nemu/src/isa/riscv32/local-include/reg.h
+++ nemu/src/isa/riscv32/local-include/reg.h
@@ -4,16 +4,16 @@
 #include <common.h>
 
 static inline int check_reg_index(int index) {
-  assert(index >= 0 && index < 32);
-  return index;
+   assert(index >= 0 && index < 32);
+   return index;
 }
 
-#define reg_l(index) (cpu.gpr[check_reg_index(index)]._32)
+#define reg_l(index) (cpu.gpr[check_reg_index(index)]._32)//目的是访问处理器的通用寄存器数组中的特定寄存器，并返回该寄存器的32位值。
 
 static inline const char* reg_name(int index) {
-  extern const char* regs[];
-  assert(index >= 0 && index < 32);
-  return regs[index];
+   extern const char* regs[];
+   assert(index >= 0 && index < 32);
+   return regs[index];
 }
 
 #endif
