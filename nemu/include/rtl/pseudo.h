4c22376d23974cf2865c7990dc0b62c10cddfc65
TBZ <401219981@qq.com>
2024-03-06 10:10:48 +0000

    modified:   nemu/include/rtl/pseudo.h

--- nemu/include/rtl/pseudo.h
+++ nemu/include/rtl/pseudo.h
@@ -33,7 +33,7 @@ static inline def_rtl(sext, rtlreg_t* dest, const rtlreg_t* src1, int width) {//
    // dest <- signext(src1[(width * 8 - 1) .. 0])
    //TODO();
    // 假设 rtlreg_t 是一个32位的类型，且width以位为单位
-   *s0 = 32 - width; // 计算需要左移然后右移的位数
+   *s0 = 32 - width * 8; // 计算需要左移然后右移的位数
    // 先将src1左移，丢弃高位，然后算术右移回来，进行符号扩展
    *dest = (rtlreg_t)(((int32_t)(*src1) << *s0) >> *s0);
 }
@@ -42,7 +42,7 @@ static inline def_rtl(zext, rtlreg_t* dest, const rtlreg_t* src1, int width) {//
    // dest <- zeroext(src1[(width * 8 - 1) .. 0])
    //TODO();
    // 创建一个掩码，它在低 width 位为 1，其他位为 0。
-   *s0 = (1ULL << width) - 1;
+   *s0 = (1ULL << (width * 8)) - 1;
    // 将 src1 的值与掩码进行 AND 操作，然后赋值给 dest。
    *dest = *src1 & *s0;
 }
@@ -51,7 +51,7 @@ static inline def_rtl(msb, rtlreg_t* dest, const rtlreg_t* src1, int width) {//m
    // dest <- src1[width * 8 - 1]
    //TODO();
    // 由于rtlreg_t是无符号32位整型，所以这里不需要担心符号扩展的问题
-    // 计算位移量，以便将感兴趣的MSB位移到最低位
+   // 计算位移量，以便将感兴趣的MSB位移到最低位
    int shift = 8 * width - 1;
 
    // 将src1右移，使目标MSB位于最低位，然后与1进行逻辑与操作
