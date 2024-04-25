a723dfb996cdabf40c45612c9a2665e0db633411
iypeep <401219981@qq.com>
2024-04-25 07:42:58 +0000

    modified:   am-kernels/tests/cpu-tests/tests/string.c

--- am-kernels/tests/cpu-tests/tests/string.c
+++ am-kernels/tests/cpu-tests/tests/string.c
@@ -1,19 +1,18 @@
 #include "trap.h"
-#include <string.h>
 
-char* s[] = {
-	"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-	"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab",
-	"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-	", World!\n",
-	"Hello, World!\n",
-	"#####"
-};
+char *s[] = {
+	 "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+	 "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab",
+	 "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+	 ", World!\n",
+	 "Hello, World!\n",
+	 "#####"};
 
 char str1[] = "Hello";
 char str[20];
 
-int main() {
+int main()
+{
 	check(strcmp(s[0], s[2]) == 0);
 	check(strcmp(s[0], s[1]) < 0);
 	check(strcmp(s[0] + 1, s[1] + 1) < 0);
