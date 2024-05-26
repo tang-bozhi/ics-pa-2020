21b5ca62a6701ed8605f481b45bb648c820f8d61
tbz <401219918@qq.com>
2024-05-26 17:18:40 +0800

    modified:   abstract-machine/klib/src/stdlib.c
    Changes not staged for commit:
    	modified:   .gitignore
    	modified:   fceux-am (modified content)

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -30,94 +30,27 @@ int atoi(const char* nptr) {
 }
 
 //下方是malloc和free的实现
-// 定义空闲块结构
-typedef struct FreeBlock {
-  size_t size;
-  struct FreeBlock* next;
-} FreeBlock;
+// 用来追踪当前分配位置的指针
+static char* addr;
 
-static FreeBlock* free_list;
-static char* heap_start;
-static char* heap_end;
-
-// 初始化堆
-void heap_init(Area heap) {
-  heap_start = (char*)heap.start;
-  heap_end = (char*)heap.end;
-  free_list = (FreeBlock*)heap_start;
-  free_list->size = heap_end - heap_start - sizeof(FreeBlock);
-  free_list->next = NULL;
+void malloc_init() {
+  // 初始化 addr 为堆的起始地址
+  addr = (char*)heap.start;
 }
 
 void* malloc(size_t size) {
-  if (size == 0) return NULL;
-
-  FreeBlock** best_fit = NULL;
-  FreeBlock** curr = &free_list;
-
-  // 找到最适合的空闲块
-  while (*curr) {
-    if ((*curr)->size >= size) {
-      if (!best_fit || (*curr)->size < (*best_fit)->size) {
-        best_fit = curr;
-      }
-    }
-    curr = &(*curr)->next;
+  // 检查是否有足够的内存分配请求的大小
+  if (addr + size > (char*)heap.end) {
+    return NULL;  // 没有足够空间，返回 NULL
   }
 
-  if (!best_fit) {
-    printf("Error allocating memory! Requested size: %zu\n", size);  // Debugging output
-    return NULL;  // 找不到合适的块
-  }
-
-  FreeBlock* block = *best_fit;
-  if (block->size >= size + sizeof(FreeBlock) + 1) {
-    // 如果块足够大，则拆分
-    char* allocated_memory = (char*)block + sizeof(FreeBlock);
-    size_t remaining_size = block->size - size - sizeof(FreeBlock);
-    FreeBlock* new_block = (FreeBlock*)(allocated_memory + size);
-    new_block->size = remaining_size;
-    new_block->next = block->next;
-    *best_fit = new_block;
-  }
-  else {
-    *best_fit = block->next;
-  }
-
-  return (char*)block + sizeof(FreeBlock);
+  char* ret = addr;  // 保存当前地址以返回
+  addr += size;  // 通过分配块的大小增加 addr
+  return ret;  // 返回已分配内存的起始地址
 }
 
 void free(void* ptr) {
-  if (!ptr) return;
-
-  if ((char*)ptr < heap_start || (char*)ptr >= heap_end) {
-    printf("Error: trying to free memory outside the heap range!\n");  // Debugging output
-    return;
-  }
-
-  FreeBlock* block_to_free = (FreeBlock*)((char*)ptr - sizeof(FreeBlock));
-  FreeBlock** curr = &free_list;
-
-  // 按地址顺序插入释放的块
-  while (*curr && *curr < block_to_free) {
-    curr = &(*curr)->next;
-  }
-  block_to_free->next = *curr;
-  *curr = block_to_free;
-
-  // 合并相邻的空闲块
-  curr = &free_list;
-  while (*curr && (*curr)->next) {
-    if ((char*)(*curr) + (*curr)->size + sizeof(FreeBlock) == (char*)(*curr)->next) {
-      (*curr)->size += (*curr)->next->size + sizeof(FreeBlock);
-      (*curr)->next = (*curr)->next->next;
-    }
-    else {
-      curr = &(*curr)->next;
-    }
-  }
+  // 根据指示留空
 }
-
-
 
 #endif
