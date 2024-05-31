38ff9321b9891016e3c628b0e0e9ef1fd47b47bf
tbz <401219918@qq.com>
2024-05-31 23:06:40 +0800

    abstract-machine ics2020 initialized
    abstract-machine 11059d5b6fbdb79ae2d22f4ce495ec04d20bf59e add riscv64-nemu

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -8,7 +8,7 @@ static unsigned long int next = 1;
 int rand(void) {
   // RAND_MAX assumed to be 32767
   next = next * 1103515245 + 12345;
-  return (unsigned int)(next / 65536) % 32768;
+  return (unsigned int)(next/65536) % 32768;
 }
 
 void srand(unsigned int seed) {
@@ -21,36 +21,19 @@ int abs(int x) {
 
 int atoi(const char* nptr) {
   int x = 0;
-  while (*nptr == ' ') { nptr++; }
+  while (*nptr == ' ') { nptr ++; }
   while (*nptr >= '0' && *nptr <= '9') {
     x = x * 10 + *nptr - '0';
-    nptr++;
+    nptr ++;
   }
   return x;
 }
 
-//下方是malloc和free的实现
-// 用来追踪当前分配位置的指针
-static char* addr;
-
-void malloc_init() {
-  // 初始化 addr 为堆的起始地址
-  addr = (char*)heap.start;
+void *malloc(size_t size) {
+  return NULL;
 }
 
-void* malloc(size_t size) {
-  // 检查是否有足够的内存分配请求的大小
-  if (addr + size > (char*)heap.end) {
-    return NULL;  // 没有足够空间，返回 NULL
-  }
-
-  char* ret = addr;  // 保存当前地址以返回
-  addr += size;  // 通过分配块的大小增加 addr
-  return ret;  // 返回已分配内存的起始地址
-}
-
-void free(void* ptr) {
-  // 根据指示留空
+void free(void *ptr) {
 }
 
 #endif
