2320748cbb6eba1204445c458815e8598d1e8283
tbz <401219981@qq.com>
2024-06-10 12:54:57 +0000

    modified:   nemu/src/device/alarm.c 	modified:   nemu/src/device/device.c

--- nemu/src/device/alarm.c
+++ nemu/src/device/alarm.c
@@ -2,41 +2,46 @@
 #include <sys/time.h>
 #include <signal.h>
 
-#define TIMER_HZ 60
-#define MAX_HANDLER 8
+#define TIMER_HZ 60  // 定义定时器的频率为每秒60次
+#define MAX_HANDLER 8  // 最大处理函数数量
 
+// 定义定时器处理函数的类型
 typedef void (*alarm_handler_t) ();
-static alarm_handler_t handler[MAX_HANDLER] = {};
-static int idx = 0;
-static uint32_t jiffy = 0;
+static alarm_handler_t handler[MAX_HANDLER] = {};  // 存储定时器处理函数的数组
+static int idx = 0;  // 当前已注册的处理函数数量
+static uint32_t jiffy = 0;  // 系统启动以来的滴答数
 
-void add_alarm_handle(void *h) {
-  assert(idx < MAX_HANDLER);
-  handler[idx ++] = h;
+// 添加一个定时器处理函数
+void add_alarm_handle(void* h) {
+  assert(idx < MAX_HANDLER);  // 确保没有超出处理函数的最大数量
+  handler[idx++] = h;  // 将新的处理函数添加到数组中，并增加索引
 }
 
+// 获取系统的运行时间（以秒为单位）
 uint32_t uptime() { return jiffy / TIMER_HZ; }
 
+// 定时器信号处理函数
 static void alarm_sig_handler(int signum) {
   int i;
-  for (i = 0; i < idx; i ++) {
-    handler[i]();
+  for (i = 0; i < idx; i++) {
+    handler[i]();  // 调用所有已注册的处理函数
   }
 
-  jiffy ++;
+  jiffy++;  // 每次信号处理后增加滴答数
 }
 
+// 初始化定时器
 void init_alarm() {
   struct sigaction s;
-  memset(&s, 0, sizeof(s));
-  s.sa_handler = alarm_sig_handler;
-  int ret = sigaction(SIGVTALRM, &s, NULL);
-  Assert(ret == 0, "Can not set signal handler");
+  memset(&s, 0, sizeof(s));  // 初始化sigaction结构体
+  s.sa_handler = alarm_sig_handler;  // 设置信号处理函数
+  int ret = sigaction(SIGVTALRM, &s, NULL);  // 注册虚拟定时器信号处理函数
+  Assert(ret == 0, "Can not set signal handler");  // 断言注册成功
 
-  struct itimerval it = {};
-  it.it_value.tv_sec = 0;
-  it.it_value.tv_usec = 1000000 / TIMER_HZ;
-  it.it_interval = it.it_value;
-  ret = setitimer(ITIMER_VIRTUAL, &it, NULL);
-  Assert(ret == 0, "Can not set timer");
+  struct itimerval it = {};  // 初始化时间间隔结构体
+  it.it_value.tv_sec = 0;  // 设置定时器的初次触发时间（秒）
+  it.it_value.tv_usec = 1000000 / TIMER_HZ;  // 设置定时器的初次触发时间（微秒）
+  it.it_interval = it.it_value;  // 设置定时器的重复触发时间间隔
+  ret = setitimer(ITIMER_VIRTUAL, &it, NULL);  // 启动虚拟定时器
+  Assert(ret == 0, "Can not set timer");  // 断言定时器启动成功
 }
--- nemu/src/device/device.c
+++ nemu/src/device/device.c
@@ -1,11 +1,12 @@
 #include <common.h>
 
-#ifdef HAS_IOE
+#ifdef HAS_IOE  // 如果定义了HAS_IOE，表示支持输入输出设备
 
 #include <device/alarm.h>
 #include <SDL2/SDL.h>
 #include <monitor/monitor.h>
 
+// 声明初始化各种设备的函数
 void init_alarm();
 void init_serial();
 void init_timer();
@@ -13,59 +14,69 @@ void init_vga();
 void init_i8042();
 void init_audio();
 
+// 声明用于发送按键信息的函数
 void send_key(uint8_t, bool);
+// 声明用于更新VGA显示的函数
 void vga_update_screen();
 
+// 设备更新标志，用于控制设备更新操作的执行
 static int device_update_flag = false;
 
+// 设置设备更新标志的函数
 static void set_device_update_flag() {
-  device_update_flag = true;
+  device_update_flag = true;  // 将设备更新标志置为真，表示需要进行设备更新
 }
 
+// 设备更新函数
 void device_update() {
-  if (!device_update_flag) {
+  if (!device_update_flag) {  // 如果设备更新标志未被设置，则直接返回
     return;
   }
-  device_update_flag = false;
-  vga_update_screen();
+  device_update_flag = false;  // 重置设备更新标志
+  vga_update_screen();  // 更新VGA屏幕显示
 
+  // 处理SDL事件
   SDL_Event event;
-  while (SDL_PollEvent(&event)) {
+  while (SDL_PollEvent(&event)) {  // 循环处理所有SDL事件
     switch (event.type) {
-      case SDL_QUIT:
-        nemu_state.state = NEMU_QUIT;
-        break;
-      // If a key was pressed
-      case SDL_KEYDOWN:
-      case SDL_KEYUP: {
-        uint8_t k = event.key.keysym.scancode;
-        bool is_keydown = (event.key.type == SDL_KEYDOWN);
-        send_key(k, is_keydown);
-        break;
-      }
-      default: break;
+    case SDL_QUIT:  // 如果是退出事件
+      nemu_state.state = NEMU_QUIT;  // 设置模拟器状态为退出
+      break;
+    case SDL_KEYDOWN:  // 按键按下事件
+    case SDL_KEYUP:  // 按键释放事件
+      uint8_t k = event.key.keysym.scancode;  // 获取按键的扫描码
+      bool is_keydown = (event.key.type == SDL_KEYDOWN);  // 判断是按下还是释放
+      send_key(k, is_keydown);  // 发送按键信息
+      break;
+    default: break;  // 其他事件不处理
     }
   }
 }
 
+// 清除SDL事件队列的函数
 void sdl_clear_event_queue() {
   SDL_Event event;
-  while (SDL_PollEvent(&event));
+  while (SDL_PollEvent(&event));  // 循环获取事件直到事件队列为空
 }
 
+// 初始化设备的函数
 void init_device() {
+  // 初始化各种设备
   init_serial();
   init_timer();
   init_vga();
   init_i8042();
   init_audio();
 
+  // 注册设备更新标志设置函数到定时器处理函数列表
   add_alarm_handle(set_device_update_flag);
+  // 初始化定时器
   init_alarm();
 }
 #else
 
+// 如果未定义HAS_IOE，定义一个空的init_device函数
 void init_device() {
 }
 
-#endif	/* HAS_IOE */
+#endif  /* HAS_IOE */
