da79c9af1fc36e209554905a6980838133aaa842
tbz <401219981@qq.com>
2024-06-30 02:24:51 +0000

    modified:   abstract-machine/am/src/nemu/ioe/input.c

--- abstract-machine/am/src/nemu/ioe/input.c
+++ abstract-machine/am/src/nemu/ioe/input.c
@@ -6,5 +6,5 @@
 void __am_input_keybrd(AM_INPUT_KEYBRD_T* kbd) {
   uint32_t kc = inl(KBD_ADDR);
   kbd->keydown = kc & KEYDOWN_MASK ? true : false;
-  kbd->keycode = kc & ~KEYDOWN_MASK
+  kbd->keycode = kc & ~KEYDOWN_MASK;
 }
