9b5227000c5fcdc8fd4921eb600198d9ad138003
tbz <401219981@qq.com>
2024-06-30 02:13:11 +0000

    modified:   abstract-machine/am/src/nemu/ioe/gpu.c 	modified:   abstract-machine/klib/include/klib.h

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -7,8 +7,8 @@
 // 初始化GPU
 void __am_gpu_init() {
   int i;
-  int w = 400;  // TODO: 获取正确的宽度
-  int h = 300;  // TODO: 获取正确的高度
+  int w = inl(VGACTL_MMIO);  // TODO: 获取正确的宽度
+  int h = inl(VGACTL_MMIO + 4);  // TODO: 获取正确的高度
   uint32_t* fb = (uint32_t*)(uintptr_t)FB_ADDR;  // 指向帧缓冲区的指针
   // 初始化帧缓冲区，为每个像素赋予一个唯一的颜色值
   for (i = 0; i < w * h; i++) fb[i] = i;
