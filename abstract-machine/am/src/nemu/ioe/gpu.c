b457af5ee19f195837395aef755c043b4beca132
tbz <401219918@qq.com>
2024-07-03 21:58:57 +0800

    modified:   abstract-machine/am/src/nemu/ioe/gpu.c 	modified:   abstract-machine/am/src/nemu/ioe/timer.c 	modified:   nemu/src/device/timer.c

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -8,7 +8,7 @@
 void __am_gpu_init() {
   int i;
   int w = inl(VGACTL_ADDR) >> 16;  // TODO: 获取正确的宽度
-  int h = inw(VGACTL_ADDR);  // TODO: 获取正确的高度
+  int h = (uint64_t)inw(VGACTL_ADDR);  // TODO: 获取正确的高度
   uint32_t* fb = (uint32_t*)(uintptr_t)FB_ADDR;  // 指向帧缓冲区的指针
   // 初始化帧缓冲区，为每个像素赋予一个唯一的颜色值
   for (i = 0; i < w * h; i++) fb[i] = i;
@@ -19,7 +19,7 @@ void __am_gpu_init() {
 void __am_gpu_config(AM_GPU_CONFIG_T* cfg) {
   *cfg = (AM_GPU_CONFIG_T){
     .present = true, .has_accel = false,
-    .width = inl(VGACTL_ADDR) >> 16, .height = inw(VGACTL_ADDR),
+    .width = inl(VGACTL_ADDR) >> 16, .height = (uint64_t)inw(VGACTL_ADDR),
     .vmemsz = 0  // 视频内存大小，初始设置为0
   };
 }
--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -7,14 +7,27 @@
 #define RTC_MMIO 0xa1000048
 
 static uint32_t* rtc_port_base = NULL;
+static uint64_t boot_time = 0;
+
+static uint64_t get_time_internal() {
+  struct timeval now;
+  gettimeofday(&now, NULL);
+  uint64_t us = now.tv_sec * 1000000 + now.tv_usec;
+  return us;
+}
+
+uint64_t get_time() {
+  if (boot_time == 0) boot_time = get_time_internal();
+  uint64_t now = get_time_internal();
+  return now - boot_time;
+}
 
 static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
   assert(offset == 0 || offset == 4);
-  if (!is_write) {
-    struct timeval now;
-    gettimeofday(&now, NULL);
-    rtc_port_base[0] = now.tv_usec;
-    rtc_port_base[1] = now.tv_sec;
+  if (!is_write && offset == 4) {
+    uint64_t us = get_time();
+    rtc_port_base[0] = (uint32_t)us;
+    rtc_port_base[1] = us >> 32;
   }
 }
 
