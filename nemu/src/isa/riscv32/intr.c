76a19314e143079ad9051d962f0dce8e5eeaf22e
iypeep <401219981@qq.com>
2024-08-21 16:04:16 +0000

    modified:   nemu/src/isa/riscv32/intr.c

--- nemu/src/isa/riscv32/intr.c
+++ nemu/src/isa/riscv32/intr.c
@@ -6,7 +6,7 @@ void raise_intr(DecodeExecState* s, word_t NO, vaddr_t epc) {
    * That is, use ``NO'' to index the IDT.
    */
   cpu.csr.scause = NO;
-  cpu.csr.sepc = epc;//指向ecall的下一个指令
+  cpu.csr.sepc = epc;//当前pc
   // 从 stvec 寄存器中取出异常入口地址
   vaddr_t stvec = cpu.csr.stvec;
 
